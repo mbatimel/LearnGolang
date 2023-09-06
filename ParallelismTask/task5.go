ecfd5728cf92cfe0e68e8d33545cc5b3fc7594b0
mangushev-ildar <mangushev.ildar@bk.ru>
2023-09-06 12:11:29 +0300

    last commit

--- ParallelismTask/task5.go
+++ ParallelismTask/task5.go
@@ -1,44 +1,33 @@
 package main
 
 import (
-   "fmt"
-   
-//    "time"
+	"fmt"
+	
 )
-// Я не дорешал
-// у меня не получается я ебал того в рот
+
 
 func merge2Channels(fn func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
-	// Для контроля завершения всех горутин
-	done := make(chan bool)
-
 	for i := 0; i < n; i++ {
 		go func() {
+			
 			x1, x2 := <-in1, <-in2
 
-			// Каналы для результатов
-			ch1, ch2 := make(chan int), make(chan int)
+			
+			
+			go func() {
+				x1 = fn(x1)
+			}()
 
-			// Запускаем вычисления в параллельных горутинах
-			go func(x int, ch chan int) {
-				ch <- fn(x)
-			}(x1, ch1)
+			go func() {
+				x2 = fn(x2)
+			}()
 
-			go func(x int, ch chan int) {
-				ch <- fn(x)
-			}(x2, ch2)
-
-			// Когда оба вычисления завершены, отправляем результат в out
-			out <- <-ch1 + <-ch2
-			done <- true
+			
+			out <- x1+x2
+			
 		}()
 	}
-
-	// Ждем завершения всех горутин
-	for i := 0; i < n; i++ {
-		<-done
-	}
-	close(out)
+	
 }
 
 func main() {
@@ -56,11 +45,11 @@ func main() {
 	}
 
 	merge2Channels(fn, in1, in2, out, 5)
-
-	for v := range out {
-		fmt.Println(v)
+	
+	for i:=1; i <= 5; i++ {
+		fmt.Println(<-out)
 	}
+	
 
-	close(in1)
-	close(in2)
+	
 }
